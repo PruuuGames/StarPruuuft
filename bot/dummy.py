39e853d63b720195cd78c560b93760092f1d4c6f
Ramon Saboya <ramons.gomes@hotmail.com>
2018-07-03 03:38:16 -0300

    Add files to run game with bot vs human
    Signed-off-by: Ramon Saboya <ramons.gomes@hotmail.com>

---
+++ bot/dummy.py
@@ -0,0 +1,6 @@
+import sc2
+
+
+class Dummy(sc2.BotAI):
+    async def on_step(self, iteration):
+        pass
---
+++ run_human_locally.py
@@ -0,0 +1,22 @@
+import json
+
+from sc2 import run_game, maps, Race
+from sc2.player import Bot
+
+from bot import *
+
+
+def main():
+    with open("botinfo.json") as f:
+        info = json.load(f)
+
+    race = Race[info["race"]]
+
+    run_game(maps.get("Abyssal Reef LE"), [
+        Bot(Race.Zerg, Dummy()),
+        Bot(race, StarPruuuft())
+    ], realtime=True, game_time_limit=(60*10), save_replay_as="test.SC2Replay")
+
+
+if __name__ == '__main__':
+    main()
