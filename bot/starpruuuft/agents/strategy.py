898872b84520121d87ea248b78eb50092506f9d0
Ramon Saboya <ramons.gomes@hotmail.com>
2018-07-07 16:10:04 -0300

    Remove check delay to avoid high delay to close base
    Signed-off-by: Ramon Saboya <ramons.gomes@hotmail.com>

--- bot/starpruuuft/agents/strategy.py
+++ bot/starpruuuft/agents/strategy.py
@@ -5,7 +5,6 @@ from .agent import Agent
 from .. import utilities
 
 SUPPLY_DEPOT_DANGER_DISTANCE = 11
-SUPPLY_DEPOT_CHECK_DELAY = 20
 
 
 class StrategyAgent(Agent):
@@ -18,16 +17,13 @@ class StrategyAgent(Agent):
         self._ramp_supply_depots = []
 
     async def on_step(self, bot, iteration):
-        await self._handle_supply_depots(bot, iteration)
+        await self._handle_supply_depots(bot)
 
     # Reconhece um depot localizado na rampa
     def _handle_ramp_supply_depot(self, *args):
-        self._ramp_supply_depots.append(args[0])
+        self._ramp_supply_depots.append(args[0][0])
 
-    async def _handle_supply_depots(self, bot, iteration):
-        if iteration % SUPPLY_DEPOT_CHECK_DELAY != 0:
-            return
-
+    async def _handle_supply_depots(self, bot):
         # Lida com os depots da rampa
         ramp_depots = bot.units.filter(lambda unit: unit.tag in self._ramp_supply_depots)
         lowered_depots = ramp_depots.filter(lambda unit: unit.type_id is UnitTypeId.SUPPLYDEPOTLOWERED)
