74821f0cde12238cb889630b0276f3fedbe7f14b
Ramon Saboya <ramons.gomes@hotmail.com>
2018-07-07 05:41:23 -0300

    Fix incorrect import usage
    Signed-off-by: Ramon Saboya <ramons.gomes@hotmail.com>

--- bot/main.py
+++ bot/main.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 import sc2
 
-from .agent import *
+from .starpruuuft.agent import StrategyAgent, BaseAgent, BuilderAgent, WorkerAgent, UpgradeAgent
 
 
 class MyBot(sc2.BotAI):
@@ -14,8 +14,8 @@ class MyBot(sc2.BotAI):
     def __init__(self):
         self.agents = {}
 
-    def add_agent(self, agent):
-        self.agents[agent.name()] = agent
+    def add_agent(self, agent_):
+        self.agents[agent_.name()] = agent_
 
     def on_start(self):
         self.add_agent(StrategyAgent(self))
@@ -27,8 +27,8 @@ class MyBot(sc2.BotAI):
     async def on_step(self, iteration):
         loop = asyncio.get_event_loop()
         tasks = []
-        for agent in self.agents.values():
-            tasks.append(loop.create_task(agent.on_step(self, iteration)))
+        for agent_ in self.agents.values():
+            tasks.append(loop.create_task(agent_.on_step(self, iteration)))
         done, pending = await asyncio.wait(tasks, timeout=2.0)
         for task in pending:
             task.cancel()
