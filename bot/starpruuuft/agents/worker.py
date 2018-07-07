7fa964a69bd349f1b7677128aa01c423718b28c6
Ramon Saboya <ramons.gomes@hotmail.com>
2018-07-07 06:24:45 -0300

    Update Worker agent to match new structure
    Signed-off-by: Ramon Saboya <ramons.gomes@hotmail.com>

--- bot/starpruuuft/agents/worker.py
+++ bot/starpruuuft/agents/worker.py
@@ -1,51 +1,42 @@
-import sc2
 from sc2.constants import *
 
-from bot.starpruuuft.agent_message import AgentMessage
 from .agent import Agent
+from .. import utilities
 
 
 class WorkerAgent(Agent):
     def __init__(self, bot):
         super().__init__(bot)
 
-        self._upgrade_command_center_done = False
-
-    def _process_messages(self):
-        if len(self._messages) == 0:
-            return
-
-        for message in self._messages:
-            message_type = message[0]
-
-            if message_type is AgentMessage.UPGRADE_COMMAND_CENTER_DONE:
-                self._upgrade_command_center_done = True
-
-        self._messages = []
-
     async def on_step(self, bot, iteration):
-        """
-        :param sc2.BotAI bot:
-        :param iteration:
-        """
-
-        self._process_messages()
-
-        cc = (bot.units(UnitTypeId.COMMANDCENTER) | bot.units(UnitTypeId.ORBITALCOMMAND)).ready
-        if not cc.exists:
+        # Caso não exista CC, o agente não faz nada
+        cc = utilities.get_command_center()
+        if cc is None:
             return
-        else:
-            cc = cc.first
 
+        await self._refinery_assign_workers(bot)
+        await self._mineral_assign_workers(bot, cc)
+        await self._mineral_assign_mules()
+
+    # Verifica se alguma refinaria está sem a quantidade ideal de trabalhadores e redistribui, caso necessário
+    async def _refinery_assign_workers(self, bot):
         for refinery in bot.units(UnitTypeId.REFINERY).ready:
             if refinery.assigned_harvesters < refinery.ideal_harvesters:
                 worker = bot.workers.closer_than(20, refinery)
                 if worker.exists:
                     await bot.do(worker.random.gather(refinery))
 
+    # Envia todos os SCVs em idle para minerar
+    async def _mineral_assign_workers(self, bot, cc):
         for scv in bot.units(UnitTypeId.SCV).idle:
             await bot.do(scv.gather(bot.state.mineral_field.closest_to(cc)))
 
-        if self._upgrade_command_center_done:
-            for mule in bot.units(UnitTypeId.MULE).idle:
-                await bot.do(mule(AbilityId.HARVEST_GATHER_MULE, bot.state.mineral_field.closest_to(cc)))
+    # Envia todas as MULEs em idle para minerar
+    async def _mineral_assign_mules(self, bot, cc):
+        # Caso não tenha o upgrade do orbital, não podem haver MULEs
+        if cc is not UnitTypeId.ORBITALCOMMAND:
+            return
+
+        for mule in bot.units(UnitTypeId.MULE).idle:
+            await bot.do(mule(AbilityId.HARVEST_GATHER_MULE, bot.state.mineral_field.closest_to(cc)))
+
