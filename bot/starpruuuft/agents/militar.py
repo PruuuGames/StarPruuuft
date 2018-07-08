07f2e42721f4a160140f2761a8486d15db3371ac
dslcin <dsl@cin.ufpe.br>
2018-07-08 20:33:43 -0300

    load medivac

--- bot/starpruuuft/agents/militar.py
+++ bot/starpruuuft/agents/militar.py
@@ -21,12 +21,15 @@ class MilitarAgent(Agent):
         self._liberator = 0
         self._barracks_ready = 0
 
+        self._medivac_cargo = {}
+
     async def on_step(self, bot, iteration):
         await self._train_marine(bot)
         await self._train_marauder(bot)
         await self._train(bot, UnitTypeId.SIEGETANK, self._factory_tech, self._siege_tank, pru.SIEGE_TANK_MAX_AMOUNT)
         await self._train(bot, UnitTypeId.MEDIVAC, self._starport_reactor, self._medivac, pru.MEDIVAC_MAX_AMOUNT)
         await self._train(bot, UnitTypeId.LIBERATOR, self._starport_reactor, self._liberator, pru.LIBERATOR_MAX_AMOUNT)
+        await self._load_medivac(bot)
 
     async def _train(self, bot, unit_type, structure, current_amount, max_amount):
         if structure is None or not structure.is_ready or bot.supply_left == 0:
@@ -70,6 +73,19 @@ class MilitarAgent(Agent):
             return
 
         await self._train(bot, UnitTypeId.MARAUDER, self._barracks_tech, self._marauder, pru.MARAUDERS_MAX_AMOUNT)
+
+    async def _load_medivac(self, bot):
+        for medivac in bot.units(UnitTypeId.MEDIVAC).idle:
+            cargo = self._medivac_cargo.setdefault(medivac.tag, {
+                UnitTypeId.MARINE: 2,
+                UnitTypeId.MARAUDER: 1,
+                UnitTypeId.SIEGETANK: 1})
+            for unit_type in cargo.keys():
+                for unit in bot.units(unit_type).idle:
+                    if cargo[unit_type]:
+                        await bot.do(medivac(AbilityId.LOAD_MEDIVAC, unit))
+                        cargo[unit_type] -= 1
+                        return
 
     def _cache(self, bot):
         self._barracks_clear, self._barracks_tech, self._barracks_reactor = utilities.get_barracks(bot)
